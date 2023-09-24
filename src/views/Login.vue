ccb09684007f63442de3a30ff16aa6e5b2b86d17
khai lee <fcxomben99@gmail.com>
2023-09-24 22:31:00 +0700

    base login

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-sm">
+    <form>
+      <h3 class="text-center mt-5">Login</h3>
+      <div class="mb-3">
+        <label class="form-label">Email address</label>
+        <input v-model="data.email" type="email" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input v-model="data.password" type="password" class="form-control" />
+      </div>
+      <button @click.prevent="login" type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+const data = reactive({
+  email: "",
+  password: "",
+});
+
+function login() {
+  console.log("email: " + data.email, "password: " + data.password);
+}
+</script>
